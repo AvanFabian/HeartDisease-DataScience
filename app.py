3e77333a5dc6439e71bd6b9ee3d1d1eddb3cefb4
AvanFabian <avanfabiandaniswara@gmail.com>
2024-05-12 20:39:31 +0700

    refactor: csv form

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request
+import pandas as pd
 
 app = Flask(__name__)
-# helloworld
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        file = request.files['file']
+        df = pd.read_csv(file)
+
+        # Get column names from the dataframe
+        columns = df.columns.tolist()
+
+        if 'target_column' in request.form:
+            target_column = request.form['target_column']
+        else:
+            target_column = columns[0]  # Set default target column to the first column
+
+        return render_template('index.html', columns=columns, selected_target=target_column)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
